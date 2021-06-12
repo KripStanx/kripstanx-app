15de738bf6077a1f653abf51afbd2b0203e37d75
Kartik <kartikkakde10@gmail.com>
2021-06-13 00:52:23 +0530

    Config Files

---
+++ src/main/webapp/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { errorRoute } from './layouts/error/error.route';
+import { navbarRoute } from './layouts/navbar/navbar.route';
+import { DEBUG_INFO_ENABLED } from 'app/app.constants';
+
+const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            [
+                ...LAYOUT_ROUTES,
+                {
+                    path: 'admin',
+                    loadChildren: './admin/admin.module#KripstanxAdminModule'
+                }
+            ],
+            { useHash: true, enableTracing: DEBUG_INFO_ENABLED }
+        )
+    ],
+    exports: [RouterModule]
+})
+export class KripstanxAppRoutingModule {}
